7a15ff2cb36a3be9602ff65d1cdef9429f6f8d85
Victor <victorherrera571@hotmail.com>
2024-12-22 23:12:08 -0800

    Feature: contact page send email now

--- my-app/src/app/contact/page.tsx
+++ my-app/src/app/contact/page.tsx
@@ -1,32 +1,70 @@
+"use client";
+import React, { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const form = useRef<HTMLFormElement>(null);
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm('service_4vtqidr', 'template_6kyo4w8', form.current, {
+          publicKey: '3SIeyrAahGsN1_EgB',
+        })
+        .then(
+          () => {
+            console.log('SUCCESS!');
+            setName('');
+            setEmail('');
+            setMessage('');
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          },
+        );
+    } else {
+      console.log('Form is not referenced.');
+    }
+  };
+
   return (
     <main>
       <h1 className="page-title">Contact</h1>
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <label htmlFor="name">Name</label>
-        <input 
-          type="text" 
-          id="name" 
-          name="name" 
-          placeholder="Name" 
-          required 
+        <input
+          type="text"
+          id="name"
+          value={name}
+          name="user_name"
+          placeholder="Name"
+          required
+          onChange={(e) => setName(e.target.value)}
         />
 
         <label htmlFor="email">Email</label>
-        <input 
-          type="email" 
-          id="email" 
-          name="email" 
-          placeholder="Email" 
+        <input
+          type="email"
+          id="email"
+          value={email}
+          name="user_email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <label htmlFor="message">Message</label>
         <textarea
           id="message"
           name="message"
+          value={message}
           placeholder="Message"
           required
+          onChange={(e) => setMessage(e.target.value)}
         ></textarea>
 
         <input type="submit" value="Submit" />
